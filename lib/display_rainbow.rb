e098ca6d8de6791263cbe39b70fe3014e338a8f2
idith almog <almog.idith@gmail.com>
2020-04-11 17:47:34 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,5 @@
 def display_rainbow (rainbowarray)
   rainbowarray=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   puts "R: #{rainbowarray(0)}, O: #{rainbowarray(1)},Y: #{rainbowarray(2)},G:#{rainbowarray(3)},B:#{rainbowarray(4)},I: #{rainbowarray(5)},V: #{rainbowarray(6)}"
-  
+
 end
