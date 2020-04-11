36f901ce213442d20b863be4beb402a522c034d0
idith almog <almog.idith@gmail.com>
2020-04-11 17:49:38 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 rainbowarray=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow (rainbowarray)
-  
+
   puts "R: #{rainbowarray(0)}, O: #{rainbowarray(1)},Y: #{rainbowarray(2)},G:#{rainbowarray(3)},B:#{rainbowarray(4)},I: #{rainbowarray(5)},V: #{rainbowarray(6)}"
 
 end
